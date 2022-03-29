9d90e21f45ae9a8c6a7bde390815d99be46a9af6
afdz204 <101666364+afdz204@users.noreply.github.com>
2022-03-29 21:06:23 +0200

    Create MenuAstros.java

---
+++ MenuAstros.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciosAbstractos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MenuAstros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("Hola, nos puede dar informacion de Un planeta o satelitre que quiera");
+        int opciones=0;
+        
+        do{
+        switch (opciones){
+            case 1:
+                break;
+        hola
+        }
+            
+        }while(opciones!=3);
+    }
+    
+}
